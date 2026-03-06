4c9dd1c2ba19681ece9c8b7d7706ec7886e2d794
Togis <sboigelot@gmail.com>
2026-03-07 00:54:45 +0100

    Test pickable object and pedestals

---
+++ Tests/PickableObjects/PickableObject.gd
@@ -0,0 +1,35 @@
+class_name PickableObject
+extends Area3D
+
+signal picked_up(pickable_object:PickableObject)
+signal dropped_down(pickable_object:PickableObject)
+
+@export var object_tag: String
+@export var scale_on_mouse_over: Vector3 = Vector3.ONE * 1.1
+
+@onready var original_parent:Node3D = get_parent()
+
+var picked:bool
+
+func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if (event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.pressed):
+		picked = true
+		picked_up.emit(self)
+		get_viewport().set_input_as_handled()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if (picked and
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.pressed):
+		picked = false
+		dropped_down.emit(self)
+		get_viewport().set_input_as_handled()
+			
+func _on_mouse_entered() -> void:
+	scale = scale_on_mouse_over
+
+func _on_mouse_exited() -> void:
+	scale = Vector3.ONE
---
+++ Tests/PickableObjects/PickableObjects.gd
@@ -0,0 +1,41 @@
+extends Node3D
+
+@export var mouse_3d_cursor:Node3D
+@export var picked_object_holder:Marker3D
+
+func _ready() -> void:
+	connect_pickable_objects()
+
+func connect_pickable_objects() -> void:
+	for pickable_object:PickableObject in get_tree().get_nodes_in_group("pickable_object"):
+		NodeHelper.connect_if_not_connected(pickable_object.picked_up, _on_pickable_object_picked_up)
+		NodeHelper.connect_if_not_connected(pickable_object.dropped_down, _on_pickable_object_dropped_down)
+
+func _on_pickable_object_picked_up(pickable_object:PickableObject) -> void:
+	pickable_object.original_parent.remove_child(pickable_object)
+	picked_object_holder.add_child(pickable_object)
+	pickable_object.position = Vector3.ZERO
+
+func _on_pickable_object_dropped_down(pickable_object:PickableObject) -> void:
+	picked_object_holder.remove_child(pickable_object)
+	pickable_object.original_parent.add_child(pickable_object)
+	pickable_object.global_position = mouse_3d_cursor.global_position
+
+func _process(_delta: float) -> void:
+	update_mouse_3d_cursor_position()
+	
+func update_mouse_3d_cursor_position() -> void:
+	var mouse_position := get_viewport().get_mouse_position()
+	var camera := get_viewport().get_camera_3d()
+	var origin := camera.project_ray_origin(mouse_position)
+	var direction := camera.project_ray_normal(mouse_position)
+	var ray_length := camera.far
+	var end := origin + direction * ray_length
+	var query := PhysicsRayQueryParameters3D.create(origin, end)
+	var space_state := get_world_3d().direct_space_state
+	var result := space_state.intersect_ray(query)
+	var collided = result.has("collider")
+	mouse_3d_cursor.visible = picked_object_holder.get_child_count() > 0 
+	if collided:
+		var mouse_position_3D = result.get("position", end)
+		mouse_3d_cursor.position = mouse_position_3D
